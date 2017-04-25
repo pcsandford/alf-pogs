1146e69b17cfb8d569c6a24b31405db55a060fd1
Peter Sandford <peter@cameroncodes.com>
2017-04-24 21:03:37 -0400

    adding in timer changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,53 @@
+const navigation = {};
+const pageChanges = {};
+
+navigation.changeInputStyle = function() {
+
+  let $this = $(this);
+
+  if($this.val().length > 0) {
+  	  $this.addClass("filled");
+  } else {
+  	 $this.removeClass("filled");
+  }
+
+};
+
+navigation.init = function() {
+
+	$(".button-great").on("click", () => console.log("yasss"));
+
+	$('input, textarea').on('keyup', function() {
+		console.log("up");
+		navigation.changeInputStyle.call($(this));
+	});
+
+};
+
+pageChanges.changeBackground = function() {
+
+	let currentTime = moment().format("HHmm");
+	let eveningTime = 2100;
+	let morningTime = 600;
+
+	if(currentTime >= eveningTime || currentTime <= morningTime) {
+		console.log(currentTime);
+		$(".intro").css({"background-color":"black","background-image":"black url('build/images/background-night.png')"});
+	} else {
+		$(".intro").css({"background-color":"white","background-image":"url('build/images/background-day.png')"});
+	}
+
+};
+
+pageChanges.init = function() {
+
+	setInterval(pageChanges.changeBackground,60000 );
+
+};
+
 $(function () {
-  $(".button-great").on("click", () => console.log("yasss"));
+
+  navigation.init();
+  pageChanges.init();
+  pageChanges.changeBackground();
 });
